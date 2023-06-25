86887188d408581f1d21b2d0b24714014d5a147b
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-06-25 11:52:58 -0300

    feat: atualizar schema de dados de saída

--- src/modules/user/dto/user-output.ts
+++ src/modules/user/dto/user-output.ts
@@ -1,6 +1,9 @@
+import { Interests } from '../repositories/adapters/user-model'
+
 export type UserOutput = {
   id: string
   email: string
   name: string
   urlProfile?: string
+  interests?: Interests[]
 }
