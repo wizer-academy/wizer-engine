0649c6e28e9763f1acf4572558684a37bb238f98
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-06-25 15:21:28 -0300

    docs: adicionar dados de temas salvos para consultas

---
+++ src/modules/user/repositories/implementations/data-technology-interests.ts
@@ -0,0 +1,33 @@
+export const technologyInterests = [
+  {
+    id: '3b9d7079-a962-46ce-934c-566ad3d6b693',
+    name: 'Desenvolvimento Web',
+  },
+  { id: '9c801066-a5e4-4e24-936d-9d708468e7cf', name: 'Data Science' },
+  {
+    id: '9006786b-413a-4e93-9e4c-ae877a749307',
+    name: 'Internet das Coisas',
+  },
+  { id: '41fd5bcf-b540-472e-ad72-e2f333d4d497', name: 'Blockchain' },
+  {
+    id: '6aa95dad-f238-46bc-8996-965a24a31d82',
+    name: 'Inteligência Artificial',
+  },
+  {
+    id: '5b850237-53c9-467f-92cc-e2a20b5368c9',
+    name: 'Desenvolvimento Mobile',
+  },
+  {
+    id: '9acdc1d2-21dc-41eb-9802-bae7a1b13a00',
+    name: 'Redes e Infraestrutura',
+  },
+  {
+    id: '5bd93701-f50e-4847-9bb9-d97354e54266',
+    name: 'Cloud Computing',
+  },
+  {
+    id: '6b2d9fb9-9c97-450b-b291-f8038d106985',
+    name: 'Segurança da Informação',
+  },
+  { id: 'af164984-3585-48ad-95ac-8bddb1008b5c', name: 'Big Data' },
+]
