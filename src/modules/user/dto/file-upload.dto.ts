652b8050d70a3186ab6661419b4a53aba68a6392
Johnson <108435026+Sjhns@users.noreply.github.com>
2023-07-02 01:26:31 -0300

    refactor: alterar nome de classe para maior descrição de sua finalidade

--- src/modules/user/dto/file-upload.dto.ts
+++ src/modules/user/dto/file-upload.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger'
 
-export class FileUploadDto {
+export class FileUploadSwagger {
   @ApiProperty({ type: 'string', format: 'binary' })
   photo: any
 }
